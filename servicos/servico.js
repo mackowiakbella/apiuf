96781ac49b35e31beda36a06f69a959fb11399ec
mackowiakbella <isabellamdallabrida@gmail.com>
2024-11-28 17:02:46 -0400

    apiuf

--- servicos/servico.js
+++ servicos/servico.js
@@ -1,14 +1,14 @@
-import colecaoUf from './dados/dados.js';
+import  colecaoUf  from '../dados/dados.js';
 
 export const buscarUfs = () => {
-    return colecaoUf;
+  return colecaoUf;
 }
 
 export const buscarUfsPorNome = (nomeUf) => {
-    return colecaoUf.filter(uf => uf.nome.toLocalLowerCase().includes(nomeUf.toLocalLowerCase()));
+  return colecaoUf.filter(uf => uf.nome.toLowerCase().includes(nomeUf.toLowerCase()));
 };
 
 export const buscarUfPorId = (id) => {
-    const idUF = parseInt(id);
-    return colecaoUf.find(uf => uf.id === idUF);
+  const idUF = parseInt(id);
+  return colecaoUf.find(uf => uf.id === idUF);
 }
